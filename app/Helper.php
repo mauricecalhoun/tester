e1fe8e82ebff07db2c1d5d805ba0ef4ae0eeb80f
mauricecalhoun <mauricecalhoun@gmail.com>
2018-04-30 08:55:20 -0500

    Wrapped Helper Function
    Checked to see if function are already declared

--- app/Helper.php
+++ app/Helper.php
@@ -1,18 +1,25 @@
 <?php
 
-function experiment($name, $data = [])
-{
-  return app()->make(\Calhoun\AB\ABTester::class)->experiment($name, $data)->run();
+if( ! function_exists('experiment') ){
+  function experiment($name, $data = [])
+  {
+    return app()->make(\Calhoun\AB\ABTester::class)->experiment($name, $data)->run();
+  }
 }
 
-function track($referer, $pathInfo)
-{
-  app()->make(\Calhoun\AB\ABTester::class)->track($referer, $pathInfo);
+
+if( ! function_exists('track') ){
+  function track($referer, $pathInfo)
+  {
+    app()->make(\Calhoun\AB\ABTester::class)->track($referer, $pathInfo);
+  }
 }
 
-function report($nomenclature)
-{
-  return app()->make(\Calhoun\AB\ABTester::class)->report($nomenclature);
+if( ! function_exists('report') ){
+  function report($nomenclature)
+  {
+    return app()->make(\Calhoun\AB\ABTester::class)->report($nomenclature);
+  }
 }
 
 ?>
